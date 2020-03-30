d1d1c542bfa21a955bdf2edd6ff3f16b546ec11c
vzdrizhni <vzdrizhni@gmail.com>
2020-03-30 18:22:01 +0500

    Added get info method

--- bin/main.rb
+++ bin/main.rb
@@ -2,16 +2,28 @@ require 'nokogiri'
 require 'open-uri'
 
 class Scrapper
+
+    attr_reader :guitars
+
     def initialize (link)
         @link = link
         @doc = Nokogiri::HTML(open(@link))
-        @product = @doc.css('li.tiles__tile')
+        @product = @doc.css('.prodbtm')
+        @guitars = []
     end
 
-    def print
-        p @product
+    def get_info
+        @product.each_with_index do |elem, index|
+            guitar = Hash.new(index)
+            guitar['img_url'] = elem.css('img').attr('src').text
+            guitar['title'] = elem.css('.product-title').text
+            guitar['price'] = elem.css('.catPagePrice').text.delete "\n"
+            @guitars << guitar
+            @guitars
+        end
     end
 end
 
-page = Scrapper.new('https://reverb.com/marketplace/electric-guitars?condition=used&page=1')
-page.print
+page = Scrapper.new('https://www.replayguitar.com/collections/electric-guitars')
+page.get_info
+p page.guitars
