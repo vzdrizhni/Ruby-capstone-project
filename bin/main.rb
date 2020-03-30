9f8bb6b510efc0f494f2b5537b639b74b4e225b2
vzdrizhni <vzdrizhni@gmail.com>
2020-03-30 22:46:28 +0500

    Made get_info method scrapp all elements from all pages

--- bin/main.rb
+++ bin/main.rb
@@ -3,27 +3,39 @@ require 'open-uri'
 
 class Scrapper
 
-    attr_reader :guitars
+    attr_reader :guitars, :link
 
     def initialize (link)
         @link = link
         @doc = Nokogiri::HTML(open(@link))
         @product = @doc.css('.prodbtm')
+        @max_page = @doc.css('#content > div > div > div.span9 > div.grid-item.pagination-border-top > div > div > div > ul > li:nth-child(6) > a').text.to_i
         @guitars = []
     end
 
     def get_info
-        @product.each_with_index do |elem, index|
-            guitar = Hash.new(index)
-            guitar['img_url'] = elem.css('img').attr('src').text
-            guitar['title'] = elem.css('.product-title').text
-            guitar['price'] = elem.css('.catPagePrice').text.delete "\n"
-            @guitars << guitar
-            @guitars
+        page_number = 1
+        number = 1
+        while page_number <= @max_page
+            @link.concat("?page=#{page_number}")
+            @doc = Nokogiri::HTML(open(@link))
+            @product = @doc.css('.prodbtm')
+            @product.each_with_index do |elem, index|
+                guitar = Hash.new(index)
+                guitar['number'] = number += 1
+                guitar['img_url'] = elem.css('img').attr('src').text
+                guitar['title'] = elem.css('.product-title').text
+                guitar['price'] = elem.css('.catPagePrice').text.delete "\n"
+                @guitars << guitar
+            end
+            @link = @link[0..56]
+            page_number += 1
         end
     end
 end
 
 page = Scrapper.new('https://www.replayguitar.com/collections/electric-guitars')
 page.get_info
-p page.guitars
+puts page.guitars
+#puts page.link
+#p 'https://www.replayguitar.com/collections/electric-guitars'.length
